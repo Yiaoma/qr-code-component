8874ea000fb0338158350661cffd3cda6b6e2f3f
Artiom Kalugin <a.o.kalugin@gmail.com>
2022-06-09 13:54:50 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Home } from 'views';
+import Style from './style';
+
+export const App = () => (
+  <>
+    <Style />
+    <Home />
+  </>
+);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'module-[contenthash].js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      // If svgs are used in project.
+      // {
+      //   test: /\.svg$/,
+      //   use: ['@svgr/webpack', 'url-loader'],
+      // },
+      // If images are used in project.
+      // {
+      //   test: /\.(png|jpe?g|gif|webp)$/,
+      //   type: 'asset/resource',
+      // },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      favicon: path.resolve(__dirname, 'src/favicon.ico'),
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      assests: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      views: path.resolve(__dirname, 'src/views'),
+      themes: path.resolve(__dirname, 'src/themes'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+    },
+  },
+};
